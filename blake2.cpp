3be5f18d8d9bf4c39a2e1f5d137dc9cfc76e901d
Rob J Meijer <pibara@gmail.com>
2015-10-20 00:41:03 +0200

    more outline of wrapper

--- blake2.cpp
+++ blake2.cpp
@@ -1,1 +1,63 @@
 #include "blake2.hpp"
+#include <blake2.h>
+#include <string>
+namespace blake2pp {
+  namespace encoding {
+    std::string Base32::encode(uint8_t *out) {
+       return "Bogus";
+    }
+    std::string Hex::encode(uint8_t *out) {
+       return "Bogus";
+    }
+  }
+  namespace libb2 {
+    int state<blake2pp::architecture::I32, blake2pp::cores::Single>::init() {
+      return blake2s_init(&s,32);
+    } 
+    int state<blake2pp::architecture::I32, blake2pp::cores::Multi>::init() {
+      return blake2sp_init(&s,32);
+    }    
+    int state<blake2pp::architecture::I64, blake2pp::cores::Single>::init() {
+      return blake2b_init(&s,32);
+    }
+    int state<blake2pp::architecture::I64, blake2pp::cores::Multi>::init() {
+      return blake2bp_init(&s,32);
+    }
+    int state<blake2pp::architecture::I32, blake2pp::cores::Single>::init_key(const void *key) {
+      return blake2s_init_key(&s,32,key,32);
+    }
+    int state<blake2pp::architecture::I32, blake2pp::cores::Multi>::init_key(const void *key) {
+      return blake2sp_init_key(&s,32,key,32);
+    }
+    int state<blake2pp::architecture::I64, blake2pp::cores::Single>::init_key(const void *key) {
+      return blake2b_init_key(&s,32,key,32);
+    }
+    int state<blake2pp::architecture::I64, blake2pp::cores::Multi>::init_key(const void *key) {
+      return blake2bp_init_key(&s,32,key,32);
+    }
+    int state<blake2pp::architecture::I32, blake2pp::cores::Single>::update(const uint8_t *in, size_t inlen) {
+      return blake2s_update(&s,in,inlen);
+    }
+    int state<blake2pp::architecture::I32, blake2pp::cores::Multi>::update(const uint8_t *in, size_t inlen) {
+      return blake2sp_update(&s,in,inlen);
+    }
+    int state<blake2pp::architecture::I64, blake2pp::cores::Single>::update(const uint8_t *in, size_t inlen) {
+      return blake2b_update(&s,in,inlen);
+    }
+    int state<blake2pp::architecture::I64, blake2pp::cores::Multi>::update(const uint8_t *in, size_t inlen) {
+      return blake2bp_update(&s,in,inlen);
+    }
+    int state<blake2pp::architecture::I32, blake2pp::cores::Single>::dofinal(uint8_t *out) {
+      return blake2s_final(&s,out,32);
+    }
+    int state<blake2pp::architecture::I32, blake2pp::cores::Multi>::dofinal(uint8_t *out) {
+      return blake2sp_final(&s,out,32);
+    }
+    int state<blake2pp::architecture::I64, blake2pp::cores::Single>::dofinal(uint8_t *out) {
+      return blake2b_final(&s,out,32);
+    }
+    int state<blake2pp::architecture::I64, blake2pp::cores::Multi>::dofinal(uint8_t *out) {
+      return blake2bp_final(&s,out,32);
+    }
+  }
+}
--- blake2.hpp
+++ blake2.hpp
@@ -2,23 +2,93 @@
 #include <string>
 namespace blake2pp {
   namespace architecture {
+    struct I32 {};
     struct I64 {}; //Selection class for 64 bit architecture variants of blake2.
   }
   namespace cores {
+    struct Single {};
     struct Multi {}; //selection class for multi-core variants of blake2.
   }
   namespace encoding {
-    struct Base32 {};  //selection class for Base32 eincoding of output hashes.
+    struct Base32 {
+      static std::string encode(uint8_t *out); 
+    };  //selection class for Base32 eincoding of output hashes.
+    struct Hex {
+      static std::string encode(uint8_t *out);
+    };
   }
-  template <typename CharType, size_t Keysize , size_t Outputsize, typename Architecture,typename Multicore, typename Encoding,typename traits=std::char_traits<CharType> >
+  namespace libb2 {
+    template <typename Architecture,typename Multicore>
+    struct state {};
+
+    template <>
+    struct state<blake2pp::architecture::I32, blake2pp::cores::Single> {
+        int init();
+        int init_key(const void *key);
+        int update(const uint8_t *in, size_t inlen);
+        int dofinal(uint8_t *out);
+      private:
+        blake2s_state s;
+    };
+
+    template <>
+    struct state<blake2pp::architecture::I32, blake2pp::cores::Multi> {
+        int init();
+        int init_key(const void *key);
+        int update(const uint8_t *in, size_t inlen);
+        int dofinal(uint8_t *out);
+      private:
+        blake2sp_state s;
+    };
+
+
+    template <>
+    struct state<blake2pp::architecture::I64, blake2pp::cores::Single> {
+        int init();
+        int init_key(const void *key);
+        int update(const uint8_t *in, size_t inlen);
+        int dofinal(uint8_t *out);
+      private:
+        blake2b_state s;
+    };
+    
+    template <>
+    struct state<blake2pp::architecture::I64, blake2pp::cores::Multi> {
+        int init();
+        int init_key(const void *key);
+        int update(const uint8_t *in, size_t inlen);
+        int dofinal(uint8_t *out);
+      private:
+        blake2bp_state s;
+    };
+    
+  }
+  template <typename Architecture,typename Multicore, typename Encoding >
   struct blake2 {
-    blake2();
-    blake2(std::basic_string<CharType,traits> key);
-    ~blake2();
-    void update(std::basic_string<CharType,traits> in);
-    operator std::basic_string<CharType,traits>();
+      blake2() { state.init(); }
+      blake2(std::string key) {state.init_key(static_cast<const void *>(key.c_str()));}
+      ~blake2(){}
+      void update(std::string in) {
+        if (res == "") {
+          state.update(static_cast<const void *>(in.c_str()),in.size());
+        }
+      }
+      operator std::string(){
+        if (res == "") {
+          uint8_t out[32];
+          state.dofinal(out);
+          res= Encoding::encode(out);
+        }
+        return res;
+      }
+    private:
+      std::string res;
+      blake2pp::libb2::state<Architecture,Encoding> state;
   };
 
-  typedef blake2pp::blake2<char, 32, 32, blake2::architecture::I64, blake2::cores::Multi, blake2::encoding::Base32 > blake2bp;
+  typedef blake2pp::blake2<blake2pp::architecture::I32, blake2pp::cores::Single, blake2pp::encoding::Base32 > blake2s;
+  typedef blake2pp::blake2<blake2pp::architecture::I32, blake2pp::cores::Multi, blake2pp::encoding::Base32 > blake2sp;
+  typedef blake2pp::blake2<blake2pp::architecture::I64, blake2pp::cores::Single, blake2pp::encoding::Base32 > blake2b;
+  typedef blake2pp::blake2<blake2pp::architecture::I64, blake2pp::cores::Multi, blake2pp::encoding::Base32 > blake2bp;
   
 }
