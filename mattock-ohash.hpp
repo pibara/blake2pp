244a6299e7dca64a424674f79622eebaf47d63d8
Rob J Meijer <pibara@gmail.com>
2015-10-21 12:26:28 +0200

    added copyright and more comments

--- mattock-ohash.hpp
+++ mattock-ohash.hpp
@@ -1,3 +1,38 @@
+//Copyright (c) 2015, Rob J Meijer.
+//Copyright (c) 2015, University College Dublin
+//All rights reserved.
+//
+//Redistribution and use in source and binary forms, with or without
+//modification, are permitted provided that the following conditions are met:
+//1. Redistributions of source code must retain the above copyright
+//   notice, this list of conditions and the following disclaimer.
+//2. Redistributions in binary form must reproduce the above copyright
+//   notice, this list of conditions and the following disclaimer in the
+//   documentation and/or other materials provided with the distribution.
+//3. All advertising materials mentioning features or use of this software
+//   must display the following acknowledgement:
+//   This product includes software developed by the <organization>.
+//4. Neither the name of the <organization> nor the
+//   names of its contributors may be used to endorse or promote products
+//   derived from this software without specific prior written permission.
+//
+//THIS SOFTWARE IS PROVIDED BY <COPYRIGHT HOLDER> ''AS IS'' AND ANY
+//EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+//WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+//DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+//DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+//(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+//LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+//ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+//(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+//SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE
+//
+//This library is primarily meant as opportunistic hashing core for the MattockFS forensic 
+//computing file-system. It implements the logic for implementing SHA and BLAKE2BP based
+//opportunistic hashing in user-space file-systems. 
+//
+#ifndef _MATTOCK_OHASH_HPP_
+#define _MATTOCK_OHASH_HPP_
 #include <blake2.h>
 #include <cryptopp/sha.h>
 #include <string>
@@ -108,6 +143,7 @@ class hash {
         //Anything else we must ignore.
       }  
     }
+    //This method must be called only after c_offset() equals the final file size.
     void done() {
        byte r[Hash::DIGESTSIZE];
        char hex[Hash::DIGESTSIZE*2];
@@ -117,17 +153,22 @@ class hash {
        }
        res=std::string(hex,Hash::DIGESTSIZE*2);
     }
+    //The current internal offset for our opportunistic hashing. Don't call done() untill the offset
+    //indicated here equates the size of the actual file entity.
     uint64_t c_offset(){
       return offset;
     }
+    //Get the hexadecimal representation of the hash.
     std::string result() {
       return res;
     }
 };
 }
 
+//A set of convenient typedefs. One for each supported hash transtition state.
 typedef mattock::opportunistic::hash<mattock::compatibility::BLAKE2BP> ohash;
 typedef mattock::opportunistic::hash<mattock::compatibility::TRANSITIONAL> ohash_transitional;
 typedef mattock::opportunistic::hash<CryptoPP::SHA> ohash_legacy;
  
 }
+#endif
