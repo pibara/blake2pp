ec3a0af82292709d7e5fa0887dc14700b1698b87
Rob J Meijer <pibara@gmail.com>
2015-10-21 11:12:33 +0200

    first working test

--- mattock-ohash.hpp
+++ mattock-ohash.hpp
@@ -2,6 +2,7 @@
 #include <cryptopp/sha.h>
 #include <string>
 #include <stdint.h>
+#include <cstdio>
 
 namespace mattock {
 namespace compatibility {
@@ -23,6 +24,30 @@ namespace compatibility {
         blake2bp_init(&s,DIGESTSIZE);
       }
   };
+  //Simple helper class for making a transitional BLAKE2BP/SHA1 comined hash behave somewhat the same 
+  //as the Crypto++ hash classes.
+  class TRANSITIONAL {
+    blake2bp_state s;
+    CryptoPP::SHA s2;
+    public:
+      enum {DIGESTSIZE = 52};
+      TRANSITIONAL():s(){
+        blake2bp_init(&s,32);
+      }
+      void Update(const byte *input, size_t length){
+        blake2bp_update(&s,input,length);
+        s2.Update(input,length);
+      }
+      void Final(byte *digest){
+        blake2bp_final(&s,digest,32); //Write the BLAKE2BP hash first.
+        s2.Final(digest+32); //Than the SHA1 after that.
+      }
+      void Restart() {
+        blake2bp_init(&s,DIGESTSIZE);
+        s2.Restart();
+      }
+  };
+
 }
 
 namespace opportunistic {
@@ -32,31 +57,26 @@ class hash {
     uint64_t offset;
     bool isnew;
     bool issparse;
-    byte r[Hash::DIGESTSIZE];
+    std::string res;
   public:
-    hash(bool anew=false,bool asparse=false):h(),offset(0),isnew(anew),issparse(asparse && anew){
-       memset(r,0,Hash::DIGESTSIZE);
-    }
+    hash(bool anew=false,bool asparse=true):h(),offset(0),isnew(anew),issparse(asparse && anew),res("BOGUS-HASH"){}
     void sparse(uint64_t len) {
         byte sparsenull[1024];
         memset(sparsenull,0,len);
         h.Update(sparsenull,len);
         offset += len;
     }
-    void chunk(char *buf, size_t size, uint64_t off){
+    //The opportunistic hashing function will be fed chunks as user processes read/write to/from the pseudo file.
+    void chunk(const char *buf, size_t size, uint64_t off){
       if (isnew) {
         //We are processing a file that is just being created.
-        //If the offset is 0 and something happened before, we need to restart.
+        //If the offset is 0 and something happened before, we need to restart and ignore our previous work.
         if (off < offset) {
             h.Restart();
             offset=0;
         }
-        if (off == offset) {
-          //We have an exact sequential match, we can always update here.
-          h.Update(buf,size);
-          offset = off + size;
-        } else {
-          //No seamlessness here, ignore or sparse.
+        if (off > offset) {
+          //There is a gap. 
           if (issparse) {
             //The file is being written sequentially but sparse, lets process the sparse data as zeroes.
             uint64_t difference = off - offset;
@@ -66,15 +86,22 @@ class hash {
             }
             sparse(difference % 1024);
           }
+          //If not sparse, than ignore this chunk.
+        }
+        if (off == offset) {
+          //We have an exact sequential match (possibly resulting from rewind or sparse filling). 
+          //We can update here using our chunk data.
+          h.Update(reinterpret_cast<const byte *>(buf),size);
+          offset = off + size;
         }
       } else {
         //We are processing an immutable existing file; this should be easy.
         if (off <= offset and (off+size) > offset) {
           //Fragment overlaps our offset; find the part that we didn't process yet.
-          char *buffseg=buf + offset - off;
+          const char *buffseg=buf + offset - off;
           size_t chunksize = size - (offset - off);
           //Process the yet unprocessed data chunk.
-          h.Update(buffseg,chunksize);
+          h.Update(reinterpret_cast<const byte *>(buffseg),chunksize);
           //Update our offset.
           offset = off + size;
         }
@@ -82,18 +109,25 @@ class hash {
       }  
     }
     void done() {
+       byte r[Hash::DIGESTSIZE];
+       char hex[Hash::DIGESTSIZE*2];
        h.Final(r);
+       for (int i=0;i<Hash::DIGESTSIZE;i++) {
+         std::sprintf(hex+2*i, "%02X", r[i]);
+       }
+       res=std::string(hex,Hash::DIGESTSIZE*2);
     }
     uint64_t c_offset(){
       return offset;
     }
-    byte *result() {
-      return r;
+    std::string result() {
+      return res;
     }
 };
 }
 
 typedef mattock::opportunistic::hash<mattock::compatibility::BLAKE2BP> ohash;
+typedef mattock::opportunistic::hash<mattock::compatibility::TRANSITIONAL> ohash_transitional;
 typedef mattock::opportunistic::hash<CryptoPP::SHA> ohash_legacy;
  
 }
--- sequentialread.cpp
+++ sequentialread.cpp
@@ -1,8 +1,44 @@
 #include "mattock-ohash.hpp"
 #include <iostream>
 
+template <typename OHash>
+std::string sequential() {
+  OHash oh;
+  char buf[128];
+  for (int index=0;index<128;index++) {
+    buf[index]=index;
+  }
+  for (int i=0;i<4096;i+=128) {
+    oh.chunk(buf,128,i);
+  };
+  oh.done();
+  return oh.result();
+  std::cout << "hash=" << oh.result() << std::endl;
+}; 
+
 int main(int argc,char **argv) {
-  mattock::ohash oh;
+  bool ok=true;
+  std::string legacyhash = sequential<mattock::ohash_legacy>();
+  std::string transitionalhash = sequential<mattock::ohash_transitional>();
+  std::string fasthash = sequential<mattock::ohash>();
+  std::string expectedlegacy="7E34C64D5AEDAB60090857BF54093AF6440001AC";
+  std::string expectednew="30E84B7FFDBBA730A3B19D1475D6E389B42BCD26873E3107E9416A9DE408B529";
+  std::string expectedtransitional = expectednew + expectedlegacy; 
+  if (legacyhash != expectedlegacy) {
+    std::cerr << "Invalid legacy hash. Expecting " << expectedlegacy << ", got " << legacyhash << std::endl;
+    ok=false;  
+  }
+  if (fasthash != expectednew) {
+    std::cerr << "Invalid fast hash. Expecting " << expectednew << ", got " << fasthash << std::endl;
+    ok=false;
+  }
+  if (transitionalhash != expectedtransitional) {
+    std::cerr << "Invalid transitional hash. Expecting " << expectedtransitional << ", got " << transitionalhash << std::endl;
+    ok=false;
+  }
+  if (ok == false) {
+    return -1;
+  }
   std::cerr << "OK" << std::endl;
   return 0;
 }
